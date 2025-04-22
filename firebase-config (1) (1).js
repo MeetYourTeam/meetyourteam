6df80ac688e04a90b865faba9bd2bb1727a2b015
MeetYourTeam <fanny.peres@outlook.fr>
2025-04-22 12:03:00 +0200

    Add files via upload

---
+++ firebase-config (1) (1).js
@@ -0,0 +1,12 @@
+
+// Configuration Firebase pour MeetYourTeam
+const firebaseConfig = {
+  apiKey: "VOTRE_API_KEY_ICI",
+  authDomain: "meetyourteam.firebaseapp.com",
+  projectId: "meetyourteam",
+  storageBucket: "meetyourteam.appspot.com",
+  messagingSenderId: "000000000000",
+  appId: "1:000000000000:web:abcdef123456"
+};
+
+export default firebaseConfig;
