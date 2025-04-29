57dddbae0d8ea22f0786a9d746be41e77cee41da
MeetYourTeam <fanny.peres@outlook.fr>
2025-04-29 14:20:05 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const startButton = document.querySelector("#start-button");
-    if (startButton) {
-        startButton.addEventListener("click", function () {
-            window.location.href = "inscription.html";
-        });
-    }
-});
